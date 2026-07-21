67de816ad74fe9ac3af785f2d3bfc649ebe388b1
Alejandro <alefelc@users.noreply.github.com>
2026-07-21 19:14:22 -0300

    Delete src directory

--- src/lib/sceneRole.ts
+++
@@ -1,3 +0,0 @@
-// Compatibility barrel. Scene-role normalization lives in the shared contract package.
-export { normalizeSceneRole } from "@te-animas/contracts";
-export type { SceneRole } from "@te-animas/contracts";
